9b0bcad7f1dad0fea364fa482ee589a1de54a6b8
steve <stevenblack1717@gmail.com>
2020-06-13 10:08:58 +0700

    fix album

--- app/src/main/java/com/chocomint/mintery/MainActivity.java
+++ app/src/main/java/com/chocomint/mintery/MainActivity.java
@@ -281,7 +281,14 @@ public class MainActivity extends AppCompatActivity implements CallbackFunction 
     }
 
     @Override
-    public void onAddPhotoSuccess() { }
+    public void onAddPhotoSuccess() {
+        Intent intent = getIntent();
+        overridePendingTransition(0, 0);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(intent);
+    }
 
     private class LoadImageAndVideo extends AsyncTask<Void, Void, Void> {
 
